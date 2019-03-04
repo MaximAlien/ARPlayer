5366ba1234f82d55fe3ad2f48f28fb68a640e84e
Maxim Makhun <maxim.makhun@gmail.com>
2019-03-04 11:01:24 +0100

    1. Code cleanup in test harness. 2. Add code coverage.

--- ARPlayerTestHarness/AppDelegate.h
+++ ARPlayerTestHarness/AppDelegate.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Maxim Makhun. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- ARPlayerTestHarness/ViewController.m
+++ ARPlayerTestHarness/ViewController.m
@@ -16,8 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
-
 
 @end
